8781f21cc5debf65f1964dcfa200c841adf68c71
vfelici <v.felici@gmail.com>
2025-08-25 11:28:15 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -257,7 +257,7 @@ app.get("/predictions/:match_id", auth, async (req, res) => {
   const userId = req.user.id; // se usi autenticazione JWT con req.user
   const { match_id } = req.params;
 
-  /*try {
+  try {
     // recupera la partita
     const matchRes = await pool.query("SELECT * FROM matches WHERE id = $1", [match_id]);
     const match = matchRes.rows[0];
@@ -284,9 +284,9 @@ app.get("/predictions/:match_id", auth, async (req, res) => {
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Errore recupero pronostici" });
-  }*/
+  }
 
-  const match = await pool.query("SELECT * FROM matches WHERE id=$1", [match_id]);
+  /*const match = await pool.query("SELECT * FROM matches WHERE id=$1", [match_id]);
     if (!match.rows.length) return res.status(404).json({ error: "Partita non trovata" });
 
     const finished = match.rows[0].finished;
@@ -305,5 +305,5 @@ app.get("/predictions/:match_id", auth, async (req, res) => {
             WHERE match_id=$1 AND user_id=$2
         `, [match_id, req.user.id]);
         res.json(pred.rows);
-    }
+    }*/
 });
